edd7b561a7041fb7b1a5f93a5237df2275b75a23
SarumanW <olgasergeevna0803@gmail.com>
2020-05-06 00:17:09 +0300

    - fix categories grid menu

--- app/src/main/java/com/example/lavka/ProductFragment.java
+++ app/src/main/java/com/example/lavka/ProductFragment.java
@@ -6,13 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProductFragment extends Fragment {
+public class ProductFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
+    GridView gridView;
+    private static final String[] mContacts = {"Рыжик", "Барсик", "Мурзик",
+            "Мурка", "Васька", "Полосатик", "Матроскин", "Лизка", "Томосина",
+            "Бегемот", "Чеширский", "Дивуар", "Тигра", "Лаура"};
 
     public ProductFragment() {
         // Required empty public constructor
@@ -22,8 +29,29 @@ public class ProductFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product, container, false);
+        View v = inflater.inflate(R.layout.fragment_product, container, false);
+        gridView = v.findViewById(R.id.gridView);
+        setupAdapter();
+        return v;
     }
 
+    void setupAdapter() {
+        if (getActivity() == null || gridView == null) return;
+        if (mContacts != null) {
+            gridView.setAdapter(new ArrayAdapter<>(getActivity(),
+                    R.layout.category_item, R.id.categoryText, mContacts));
+        } else {
+            gridView.setAdapter(null);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
